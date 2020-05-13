03d953827250f50df7a41f478b01235b6c79fe43
Dustin <dmkelly100@gmail.com>
2020-05-13 18:20:34 -0400

    quick fix for mobile nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '../json/particles.json', function () {
-	console.log('callback - particles.js config loaded');
-});
-
-const menuIcon = document.querySelector('.menuIcon');
-const navBar = document.querySelector('.navBar');
-const navBarLinks = document.querySelectorAll('.navBarLinks, .menuIcon');
-
-navBarLinks.forEach((item) => {
-	item.addEventListener('click', () => {
-		navBar.classList.toggle('change');
+document.addEventListener('DOMContentLoaded', function () {
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', '../json/particles.json', function () {
+		console.log('callback - particles.js config loaded');
 	});
+	
+	const menuIcon = document.querySelector('.menuIcon');
+	const navBar = document.querySelector('.navBar');
+	const navBarLinks = document.querySelectorAll('.navBarLinks, .menuIcon');
+	
+	navBarLinks.forEach((item) => {
+		item.addEventListener('click', () => {
+			navBar.classList.toggle('change');
+		});
+	});
+	
 });
