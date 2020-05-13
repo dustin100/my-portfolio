4dc5594cdf608a3f95e0bf759b5d78053b2029aa
Dustin <dmkelly100@gmail.com>
2020-05-13 15:56:40 -0400

    added nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@
 particlesJS.load('particles-js', '../json/particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
+
+
+const menuIcon = document.querySelector
+('.menuIcon')
+const navBar = document.querySelector('.navBar')
+
+menuIcon.addEventListener('click', ()=> {
+navBar.classList.toggle('change')
+})
