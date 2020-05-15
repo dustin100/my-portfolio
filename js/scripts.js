4636d2d081a810873f84d53637f3f83250eeed6f
Dustin <dmkelly100@gmail.com>
2020-05-14 23:08:53 -0400

    more styling plus added copy

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	particlesJS.load('particles-js', '../json/particles.json', function () {
 		console.log('callback - particles.js config loaded');
 	});
-	
-	const menuIcon = document.querySelector('.menuIcon');
+
 	const navBar = document.querySelector('.navBar');
 	const navBarLinks = document.querySelectorAll('.navBarLinks, .menuIcon');
-	
+
 	navBarLinks.forEach((item) => {
 		item.addEventListener('click', () => {
 			navBar.classList.toggle('change');
 		});
 	});
-	
 });
